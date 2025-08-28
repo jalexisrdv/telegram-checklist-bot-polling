81c0e09dd2af26f90cd011d8a7a93e014b2d9112
jalexisrdv <jalexisrdv@outlook.com>
2025-08-27 23:24:48 -0600

    first commit

---
+++ src/main/java/com/jardvcode/bot/TelegramChecklistBotPollingApplication.java
@@ -0,0 +1,13 @@
+package com.jardvcode.bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramChecklistBotPollingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelegramChecklistBotPollingApplication.class, args);
+	}
+
+}
